f172f0f47c68c284803cc240c8be407ec38f8855
Kaan Sentürk <skaan@student.ethz.ch>
2020-11-29 19:45:47 +0100

    Add microservice setup scripts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import multiparty from 'multiparty';
 import { AppService } from './app.service';
+import pjson from '../package.json';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('file')
+  async uploadFile(@Req() req, @Res() res) {
+    const form = new multiparty.Form();
+    form.parse(req, async (err, fields, files) => {
+      const { Location } = await this.appService.uploadFile(fields, files);
+      res.send({ url: Location })
+    })
+  }
+
+  @Get('health')
+  getVersion() {
+    return pjson.version;
   }
 }
